3dca7cd3a8e40c0db9268569626c6071de5fbaaf
Ashley Daniell <adaniell@hbs.edu>
2014-10-19 02:40:32 -0400

    working version

--- app/routes.php
+++ app/routes.php
@@ -1,53 +1,78 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
-|
-*/
-
-Route::get('/', function()
-{
-	return View::make('hello');
-});
-
-Route::get('/about', function()
-{
-	return View::make('hello');
-});
-
-Route::get('/new', function() {
-
-    $view  = '<form method="POST">';
-    $view .= 'Title: <input type="text" name="title">';
-    $view .= '<input type="submit">';
-    $view .= '</form>';
-    return $view;
-
-});
-
-Route::post('/new', function() {
-
-    $input =  Input::all();
-    print_r($input);
-
-});
-
-Route::get('flights/{day_of_week}', function($day_of_week)
-{
-	$days = array('monday', 'tuesday');
-
-	if(in_array(strtolower($day_of_week), $days)) {
-		return "Here are the flights departing on $day_of_week...";
-	}
-	else {
-		return "invalid day";
-	}
-
+// Homepage
+Route::get('/', function() {
 	
+    return View::make('index');
+
 });
+
+// List all books / search
+Route::get('/list/{format?}', function($format = 'html') {
+
+    $query = Input::get('query');
+
+    $library = new Library();
+    $library->setPath(app_path().'/database/books.json');
+    $books = $library->getBooks();
+
+    if($query) {
+        $books = $library->search($query);
+    }
+
+    if($format == 'json') {
+        return 'JSON Version';
+    }
+    elseif($format == 'pdf') {
+        return 'PDF Version;';
+    }
+    else {
+        return View::make('list')
+            ->with('name','Susan')
+            ->with('books', $books)
+            ->with('query', $query);
+
+    }
+
+});
+
+
+// Display the form for a new book
+Route::get('/add', function() {
+
+    return View::make('add');
+
+});
+
+// Process form for a new book
+Route::post('/add', function() {
+
+});
+
+// Display the form to edit a book
+Route::get('/edit/{title}', function() {
+
+
+});
+
+// Process form for a edit book
+Route::post('/edit/', function() {
+
+
+});
+
+// Test route to load and output books
+Route::get('/data', function() {
+
+    $library = new Library();
+
+    $library->setPath(app_path().'/database/books.json');
+    
+    $books = $library->getBooks();
+
+    // Return the file
+    echo Pre::render($books);
+
+});
+
+
---
+++ app/views/_master.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE HTML>
+
+<html lang="en">
+
+
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8">
+	<link rel="stylesheet" type="text/css" href="foobooks.css">
+
+	<title>@yield ('title', 'Foobooks')</title>
+
+</head>
+
+<body>
+
+<img src="/images/laravel-foobooks-logo@2x.png" alt="logo" width="500"/>
+
+@yield ('content')
+
+
+</body>
+
+
+
+</html>
+
+
