f33e7acd89e9892fe0f68ba686a5bdb8040e2ea5
Ashley Daniell <adaniell@hbs.edu>
2014-11-15 15:48:03 -0500

    database production

--- app/views/_master.blade.php
+++ app/views/_master.blade.php
@@ -1,27 +1,34 @@
-<!DOCTYPE HTML>
-
-<html lang="en">
-
-
+<!DOCTYPE html>
+<html>
 <head>
-	<meta http-equiv="content-type" content="text/html; charset=utf-8">
-	<link rel="stylesheet" type="text/css" href="foobooks.css">
 
-	<title>@yield ('title', 'Foobooks')</title>
+	<title>@yield('title','Foobooks')</title>
+	<meta charset='utf-8'>
 
+	<link rel='stylesheet' href='/css/stylesheet.css' type='text/css'>
+
+	@yield('head')
+
+	
 </head>
-
 <body>
 
-<img src="/images/laravel-foobooks-logo@2x.png" alt="logo" width="500"/>
+	<a href='/'><img class='logo' width='500' height='300' src='/images/laravel-foobooks-logo@2x.png' alt='Foobooks logo'></a>
 
-@yield ('content')
+	<br/>
 
+	<a href='https://github.com/susanBuck/foobooks'>View on Github</a>
 
+	<nav>
+		<ul>
+			<li><a href='/list'>List All</a></li>
+			<li><a href='/add'>+ Add Book</a></li>
+		</ul>
+	</nav>
+	
+	@yield('content')
+
+	@yield('body')
+	
 </body>
-
-
-
 </html>
-
-
